61b07d709c50a7d832b8f599160458bf0f86bef6
Ravneel <ravneelsingh17@gmail.com>
2025-05-14 01:32:13 -0400

    Simple project with API request

---
+++ script.js
@@ -0,0 +1,47 @@
+let button = document.querySelector('button');
+
+let input = document.querySelector('input');
+
+let ul = document.querySelector('ul');
+
+let url = 'http://universities.hipolabs.com/search?name=';
+
+
+async function getColleges(country) {
+
+      
+    let result = await axios.get(url + country);
+    
+    return result.data;
+}
+
+
+function show(colleges) {
+
+ul.innerText = ''
+
+     for (college of colleges) {
+
+         let li = document.createElement('li');
+
+         li.innerText = college.name;
+
+         ul.appendChild(li);
+     }
+     
+     input.value = '';
+}
+
+
+button.addEventListener('click', async ()=> {
+
+    let country = input.value;
+
+    let colleges = await getColleges(country);
+
+    show (colleges);
+})
+
+
+
+
